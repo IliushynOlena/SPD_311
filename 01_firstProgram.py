5a36de84d86dda717646a1dd03d042746e9bf101
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2023-12-16 17:56:57 +0200

    first commit

---
+++ 01_firstProgram.py
@@ -0,0 +1,23 @@
+#Shift+Alt+ arrow  -->
+# \n  \t  \" \\
+# print("Hello") # Ctrl+K+C
+# print("Mukola")
+# print("Petrovuch")
+# print('Hello Mukola')
+# print('Hello Mukola')
+print('Hello\nMukola\nPetrovuch')
+print('Hello friend Petrovuch')
+print('I\n\tlove\n\t\tPython')
+print('I')
+print('      love ')
+print('             Python')
+
+print("I love \"Python\"")
+print('I love "Python"')
+print('I love \\Python\\')
+print('I love /Python/')
+
+print("P","y","t","h","o","n",sep="_" , end=" ")
+print("Very good day!!!")
+
+
---
+++ 02_data_types.py
@@ -0,0 +1,145 @@
+'''
+print("Literals in Python")
+print("1000")
+print(1000)
+print(2)
+#print(S)
+print("PI")
+#Data types :
+#number : int, float
+#str
+#boolean
+print("int = ", type(22))
+print("float = ", type(3.14))
+print("str = ", type("5.6"))
+print("bool = ", type(True))
+print("bool = ", type(False))
+print(2 + 2)
+print(2 - 2)
+print(2 / 2)
+print(2 * 2)
+# + - * / // % **
+
+print(2 ** 4)
+print(2 ** 2.0)
+print(2. ** 6)
+print(2. ** 3.1)
+
+print(2 * 3)
+print(2 * 3.)
+print(2. * 3)
+print(2. * 3.)
+
+print(9 / 3)
+print(9/3.)
+print(9./3)
+print(9./3.)
+
+print(9 // 4)
+print(9 //4.)
+print(9.//4)
+print(9.//4.)
+
+print(9 % 3)
+print(15%4)
+print(18%5)
+
+print(-8 + 5 +3 - 7 + 5)
+
+print(2 + 3 * 5)
+
+print(2 ** 2 ** 3)#64   
+#2**3 = 8
+#2**8  = 256
+
+print(2 + 3.14)#5.14
+#print(5 + "str")#"5str"
+print(5 + True)#True = 1
+print(5 + False)#False = 0
+
+print(1_000_000_000)#1*10_9
+a = 10
+print(a)
+print(a)
+print(a)
+print(a)
+
+age = 15
+Age = 45
+#4number = 44
+number1 = 44
+_num = 4
+#def = 788
+age_of_human = 100
+ageOfHuman = 100
+
+variable = 1 #initialization
+variable = 333#присвоєння
+
+var = 1#int
+account_balace = 1000.0#float
+client_name = "Jack"#str
+print(var, account_balace, client_name)
+var = 200 + 300
+print(var, account_balace, client_name)
+
+first_number = 10
+second_number = 22
+result = first_number + second_number
+print(first_number + second_number)
+print(result)
+
+print(first_number)
+first_number = first_number + 200
+first_number += 200
+print(first_number)
+first_number -= 200
+print(first_number)
+first_number *= 200
+print(first_number)
+first_number /= 200
+print(first_number)
+'''
+
+line_1 = "Hyper"
+line_2 = "Text"
+line_3 = "MarkUp"
+line_4 = "Language"
+allLine = line_1 + " " + line_2 + " " + line_3 + " " + line_4
+print("===================================")
+print(allLine * 3)
+print("="*35)
+print( "\033[31m",line_1)
+print(line_1, line_2, line_3 ,line_4)
+print("\033[32m\033[45m\033[1m{}\033[0m".format(line_1),end=" ")
+print("\033[33m\033[46m\033[2m{}\033[0m".format(line_2),end=" ")
+print("\033[34m\033[47m\033[3m{}\033[0m".format(line_3),end=" ")
+print("\033[35m\033[41m\033[4m{}\033[0m".format(line_4),end=" ")
+print("\033[0m")
+#print("Enter your age")
+age = input("Enter your age")
+print(age)
+
+first_number = int(input("Enter first number : ")) 
+second_number = float(input("Enter second number : ")) 
+result = first_number + second_number
+print(result)
+
+5555//3600=1
+5555//60%60=32
+5555%60 = 35
+5555/60 = 92
+92 * 60 = 5520
+5555-5520 = 35
+
+1*3600+32*60=5520
+
+
+
+
+
+
+
+
+
+
